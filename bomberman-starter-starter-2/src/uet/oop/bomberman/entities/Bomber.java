9e0f86468c472128c9d01ef24e205215715b5ced
chienltt <19020228@vnu.edu.vn>
2020-11-05 22:20:00 +0700

    Update Bomber.java

--- bomberman-starter-starter-2/src/uet/oop/bomberman/entities/Bomber.java
+++ bomberman-starter-starter-2/src/uet/oop/bomberman/entities/Bomber.java
@@ -5,15 +5,107 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
+import uet.oop.bomberman.BombermanGame;
+import uet.oop.bomberman.graphics.Sprite;
+
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bomber extends Entity {
+    private List<Image> imgmoveleft=new ArrayList<>();
+    private List<Image> imgmoveright=new ArrayList<>();
+    private List<Image> imgmoveup=new ArrayList<>();
+    private List<Image> imgmovedown=new ArrayList<>();
+    private List<Image> imgdead=new ArrayList<>();
 
+    private Boolean hold;
+    private String keymove ;
     public Bomber(int x, int y, Image img) {
         super( x, y, img);
+
+        imgmoveleft.add(Sprite.player_left.getFxImage());
+        imgmoveleft.add(Sprite.player_left_1.getFxImage());
+        imgmoveleft.add(Sprite.player_left_2.getFxImage());
+
+        imgmoveright.add(Sprite.player_right.getFxImage());
+        imgmoveright.add(Sprite.player_right_1.getFxImage());
+        imgmoveright.add(Sprite.player_right_2.getFxImage());
+
+        imgmoveup.add(Sprite.player_up.getFxImage());
+        imgmoveup.add(Sprite.player_up_1.getFxImage());
+        imgmoveup.add(Sprite.player_up_2.getFxImage());
+
+        imgmovedown.add(Sprite.player_down.getFxImage());
+        imgmovedown.add(Sprite.player_down_1.getFxImage());
+        imgmovedown.add(Sprite.player_down_2.getFxImage());
+
+        imgdead.add(Sprite.player_dead1.getFxImage());
+        imgdead.add(Sprite.player_dead2.getFxImage());
+        imgdead.add(Sprite.player_dead3.getFxImage());
+
+        hold=false;
+        keymove="";
     }
 
+    private Image Bombermoveleft(){
+        if(img==imgmoveleft.get(0)) return imgmoveleft.get(1);
+        else if(img==imgmoveleft.get(1)) return imgmoveleft.get(2);
+        else if(img==imgmoveleft.get(2)) return imgmoveleft.get(0);
+        else return imgmoveleft.get(0);
+
+    }
+
+    private Image Bombermoveright(){
+        if(x < BombermanGame.WIDTH-2) x=x+0.3;
+        if(img==imgmoveright.get(0)) return imgmoveright.get(1);
+        else if(img==imgmoveright.get(1)) return imgmoveright.get(2);
+        else if(img==imgmoveright.get(2))
+        {
+            return imgmoveright.get(0);
+        }
+        else return imgmoveright.get(0);
+    }
+
+    private Image Bombermoveup(){
+        if(img==imgmoveup.get(0)) return imgmoveup.get(1);
+        else if(img==imgmoveup.get(1)) return imgmoveup.get(2);
+        else if(img==imgmoveup.get(2)) return imgmoveup.get(0);
+        else return imgmoveup.get(0);
+    }
+
+    private Image Bombermovedown(){
+        if(img==imgmovedown.get(0)) return imgmovedown.get(1);
+        else if(img==imgmovedown.get(1)) return imgmovedown.get(2);
+        else if(img==imgmovedown.get(2)) return imgmovedown.get(0);
+        return imgmovedown.get(0);
+    }
     @Override
     public void update() {
+        KeyListener a = new batsukien();
 
+        if(keymove=="d" && hold == true) img=Bombermoveright();
+
+    }
+    class batsukien implements KeyListener {
+        @Override
+        public void keyTyped(KeyEvent e) {
+            hold=true;
+            System.out.println(1);
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            hold=false;
+            System.out.println(2);
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            if(e.getKeyCode() == KeyEvent.VK_D) keymove ="d";
+            System.out.println(3);
+        }
     }
 }
