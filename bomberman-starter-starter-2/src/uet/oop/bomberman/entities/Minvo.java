2a410d2fe8d098650064e38537a37c84e32ebe7d
chienltt <19020228@vnu.edu.vn>
2020-11-06 15:43:20 +0700

    Create Minvo.java

---
+++ bomberman-starter-starter-2/src/uet/oop/bomberman/entities/Minvo.java
@@ -0,0 +1,14 @@
+package uet.oop.bomberman.entities;
+
+import javafx.scene.image.Image;
+
+public class Minvo extends Entity {
+    public Minvo(int x, int y, Image img) {
+        super(x, y, img);
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
