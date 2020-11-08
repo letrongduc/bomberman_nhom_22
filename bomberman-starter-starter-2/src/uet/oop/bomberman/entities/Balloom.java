3667ee01d5bc0f1422bd2a5b952c56129d5031b7
chienltt <19020228@vnu.edu.vn>
2020-11-08 12:54:45 +0700

    Update Balloom.java

--- bomberman-starter-starter-2/src/uet/oop/bomberman/entities/Balloom.java
+++ bomberman-starter-starter-2/src/uet/oop/bomberman/entities/Balloom.java
@@ -42,15 +42,14 @@ public class Balloom extends Entity {
 
     private String  getkeymoving(){
         List<String> d= Map.directionposible(x,y);
-        System.out.println(x+","+y);
-        Random generator = new Random(40);
+        Random generator = new Random();
         int key =generator.nextInt(d.size());
+        System.out.println(key);
         return d.get(key);
     }
 
     private Image Balloommoveleft(){
-        System.out.println(1);
-        if(x > 1 && Map.checkcollision(x,y,"a") ) x=x-0.2;
+        if(x > 1 && Map.checkcollision(x,y,"a",1,0.2) ) x=x-0.2;
         if(img==imgmoveleft.get(0)) return imgmoveleft.get(1);
         else if(img==imgmoveleft.get(1)) return imgmoveleft.get(2);
         else if(img==imgmoveleft.get(2)) return imgmoveleft.get(0);
@@ -58,8 +57,7 @@ public class Balloom extends Entity {
     }
 
     private Image Balloommoveright(){
-        System.out.println(2);
-        if(x < BombermanGame.WIDTH-2 && Map.checkcollision(x,y,"d") ) x=x+0.2;
+        if(x < BombermanGame.WIDTH-2 && Map.checkcollision(x,y,"d",1,0.2) ) x=x+0.2;
         if(img==imgmoveright.get(0)) return imgmoveright.get(1);
         else if(img==imgmoveright.get(1)) return imgmoveright.get(2);
         else if(img==imgmoveright.get(2)) return imgmoveright.get(0);
@@ -67,8 +65,7 @@ public class Balloom extends Entity {
     }
 
     private Image Balloommoveup(){
-        System.out.println(3);
-        if(y>1 && Map.checkcollision(x,y,"w") ) y=y-0.2;
+        if(y>1 && Map.checkcollision(x,y,"w",1,0.2) ) y=y-0.2;
         if(img==imgmoveup.get(0)) return imgmoveup.get(1);
         else if(img==imgmoveup.get(1)) return imgmoveup.get(2);
         else if(img==imgmoveup.get(2)) return imgmoveup.get(0);
@@ -76,8 +73,7 @@ public class Balloom extends Entity {
     }
 
     private Image Balloommovedown(){
-        System.out.println(4);
-        if(y<=BombermanGame.HEIGHT-2 && Map.checkcollision(x,y,"s") ) y=y+0.2;
+        if(y<=BombermanGame.HEIGHT-2 && Map.checkcollision(x,y,"s",1,0.2) ) y=y+0.2;
         if(img==imgmovedown.get(0)) return imgmovedown.get(1);
         else if(img==imgmovedown.get(1)) return imgmovedown.get(2);
         else if(img==imgmovedown.get(2)) return imgmovedown.get(0);
@@ -87,7 +83,6 @@ public class Balloom extends Entity {
         if (MovingLoopindex==5) MovingLoopindex=0;
         if (MovingLoopindex==0) keymove=getkeymoving();
         MovingLoopindex = MovingLoopindex+1;
-        System.out.println(keymove+","+MovingLoopindex);
         if(keymove=="d") img=Balloommoveright();
         if(keymove=="a") img=Balloommoveleft();
         if(keymove=="w") img=Balloommoveup();
