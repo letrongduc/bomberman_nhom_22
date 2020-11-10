dd2c97e0c01121003319dd0190d99a9daba13888
chienltt <19020228@vnu.edu.vn>
2020-11-10 21:48:06 +0700

    Update Balloom.java

--- bomberman-starter-starter-2/src/uet/oop/bomberman/entities/Balloom.java
+++ bomberman-starter-starter-2/src/uet/oop/bomberman/entities/Balloom.java
@@ -44,12 +44,11 @@ public class Balloom extends Entity {
         List<String> d= Map.directionposible(x,y);
         Random generator = new Random();
         int key =generator.nextInt(d.size());
-        System.out.println(key);
         return d.get(key);
     }
 
     private Image Balloommoveleft(){
-        if(x > 1 && Map.checkcollision(x,y,"Left",1,0.2) ) x=x-0.2;
+        if(x > 0 && Map.checkcollision(x,y,"Left",1,0.1) ) x=x-0.1;
         if(img==imgmoveleft.get(0)) return imgmoveleft.get(1);
         else if(img==imgmoveleft.get(1)) return imgmoveleft.get(2);
         else if(img==imgmoveleft.get(2)) return imgmoveleft.get(0);
@@ -57,7 +56,7 @@ public class Balloom extends Entity {
     }
 
     private Image Balloommoveright(){
-        if(x < BombermanGame.WIDTH-2 && Map.checkcollision(x,y,"Right",1,0.2) ) x=x+0.2;
+        if(x < BombermanGame.WIDTH-1 && Map.checkcollision(x,y,"Right",1,0.1) ) x=x+0.1;
         if(img==imgmoveright.get(0)) return imgmoveright.get(1);
         else if(img==imgmoveright.get(1)) return imgmoveright.get(2);
         else if(img==imgmoveright.get(2)) return imgmoveright.get(0);
@@ -65,7 +64,7 @@ public class Balloom extends Entity {
     }
 
     private Image Balloommoveup(){
-        if(y>1 && Map.checkcollision(x,y,"Up",1,0.2) ) y=y-0.2;
+        if(y>0 && Map.checkcollision(x,y,"Up",1,0.1) ) y=y-0.1;
         if(img==imgmoveup.get(0)) return imgmoveup.get(1);
         else if(img==imgmoveup.get(1)) return imgmoveup.get(2);
         else if(img==imgmoveup.get(2)) return imgmoveup.get(0);
@@ -73,17 +72,16 @@ public class Balloom extends Entity {
     }
 
     private Image Balloommovedown(){
-        if(y<=BombermanGame.HEIGHT-2 && Map.checkcollision(x,y,"Down",1,0.2) ) y=y+0.2;
+        if(y< BombermanGame.HEIGHT-1 && Map.checkcollision(x,y,"Down",1,0.1) ) y=y+0.1;
         if(img==imgmovedown.get(0)) return imgmovedown.get(1);
         else if(img==imgmovedown.get(1)) return imgmovedown.get(2);
         else if(img==imgmovedown.get(2)) return imgmovedown.get(0);
         return imgmovedown.get(0);
     }
     private void MovingLoop(){
-        if (MovingLoopindex==5) MovingLoopindex=0;
+        if (MovingLoopindex==10) MovingLoopindex=0;
         if (MovingLoopindex==0) keymove=getkeymoving();
         MovingLoopindex = MovingLoopindex+1;
-        System.out.println(keymove+","+MovingLoopindex);
         if(keymove=="Left") img=Balloommoveright();
         if(keymove=="Right") img=Balloommoveleft();
         if(keymove=="Up") img=Balloommoveup();
