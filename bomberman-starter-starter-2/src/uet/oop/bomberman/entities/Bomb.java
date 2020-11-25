3c679064d12f7f8eb6039071ba79d6b1fb28ec51
chienltt <19020228@vnu.edu.vn>
2020-11-25 09:30:35 +0700

    Update Bomb.java

--- bomberman-starter-starter-2/src/uet/oop/bomberman/entities/Bomb.java
+++ bomberman-starter-starter-2/src/uet/oop/bomberman/entities/Bomb.java
@@ -25,24 +25,98 @@ public class Bomb extends Entity{
     private List<Image> explosion_vertical_down = new ArrayList<>();
     private List<Image> explosion_vertical_top = new ArrayList<>();
     private int idbomb=1;
+    static private boolean isexploded =false;
+    private int timedelaycount =0;
+    private int timeexploded=0;
+    public boolean isEnd = false;
 
-
-    public Bomb(int x,int y,Image img){
+    public Bomb(double x,double y,Image img){
         super(x,y,img);
-        Bombsmall.add(Sprite.bomb.getFxImage());
-        Bombsmall.add(Sprite.bomb_1.getFxImage());
-
+        Bombbig.add(Sprite.bomb.getFxImage());
         Bombbig.add(Sprite.bomb_1.getFxImage());
-        Bombbig.add(Sprite.bomb_2.getFxImage());
+
+        Bombsmall.add(Sprite.bomb_1.getFxImage());
+        Bombsmall.add(Sprite.bomb_2.getFxImage());
 
         Bomb_exploded.add(Sprite.bomb_exploded.getFxImage());
         Bomb_exploded.add(Sprite.bomb_exploded1.getFxImage());
         Bomb_exploded.add(Sprite.bomb_exploded2.getFxImage());
+
+        explosion_horizontal.add(Sprite.explosion_horizontal.getFxImage());
+        explosion_horizontal.add(Sprite.explosion_horizontal1.getFxImage());
+        explosion_horizontal.add(Sprite.explosion_horizontal2.getFxImage());
+
+        explosion_horizontal_left.add(Sprite.explosion_horizontal_left_last.getFxImage());
+        explosion_horizontal_left.add(Sprite.explosion_horizontal_left_last1.getFxImage());
+        explosion_horizontal_left.add(Sprite.explosion_horizontal_left_last2.getFxImage());
+
+        explosion_horizontal_right.add(Sprite.explosion_horizontal_right_last.getFxImage());
+        explosion_horizontal_right.add(Sprite.explosion_horizontal_right_last1.getFxImage());
+        explosion_horizontal_right.add(Sprite.explosion_horizontal_right_last2.getFxImage());
+
+        explosion_vertical.add(Sprite.explosion_vertical.getFxImage());
+        explosion_vertical.add(Sprite.explosion_vertical1.getFxImage());
+        explosion_vertical.add(Sprite.explosion_vertical2.getFxImage());
+
+        explosion_vertical_down.add(Sprite.explosion_vertical_down_last.getFxImage());
+        explosion_vertical_down.add(Sprite.explosion_vertical_down_last1.getFxImage());
+        explosion_vertical_down.add(Sprite.explosion_vertical_down_last2.getFxImage());
+
+        explosion_vertical_top.add(Sprite.explosion_vertical_top_last.getFxImage());
+        explosion_vertical_top.add(Sprite.explosion_vertical_top_last1.getFxImage());
+        explosion_vertical_top.add(Sprite.explosion_vertical_top_last2.getFxImage());
+
+    }
+
+    public Image Bombpreparingexplode(){
+        if(idbomb==1){
+            if(img == Bombsmall.get(0) )return Bombsmall.get(1);
+            else return Bombsmall.get(0);
+        }
+        else
+        {
+            if(img== Bombbig.get(0)) return Bombbig.get(1);
+            else return Bombbig.get(0);
+        }
+    }
+    public Image explosion_horizontal_exploded(Image img){
+        if(img == explosion_horizontal.get(0) ) return explosion_horizontal.get(1);
+        if(img == explosion_horizontal.get(1) ) return explosion_horizontal.get(2);
+        else return explosion_horizontal.get(0);
+    }
+    public Image Bombexploded(){
+            if(img==Bomb_exploded.get(0)) return Bomb_exploded.get(1);
+            if(img==Bomb_exploded.get(1)) return Bomb_exploded.get(2);
+            else return Bomb_exploded.get(0);
+    }
+    public Image looptime(){
+        System.out.println(timeexploded);
+        System.out.println(isEnd);
+            if (timedelaycount == 16)
+            {
+                isexploded=true;
+                if(timeexploded!= 6){
+                    timeexploded=timeexploded+1;
+                    if(timedelaycount % 2 == 0 ) return Bombexploded();
+                    else return img;
+                }
+                else {
+                    isexploded=false;
+                    isEnd =true;
+                    return null;
+                }
+            }
+            else {
+                timedelaycount=timedelaycount+1;
+                if(timedelaycount % 2 == 0 ) return Bombpreparingexplode();
+                else return img;
+            }
     }
     
 
+
     @Override
     public void update() {
-
+        img = looptime();
     }
 }
