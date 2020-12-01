ebc099f8fb486b8d1085820cc974066152992537
chienltt <19020228@vnu.edu.vn>
2020-12-01 23:26:39 +0700

    Create Explosion_vertical.java

---
+++ bomberman-starter-starter-2/src/uet/oop/bomberman/entities/bombEffect/Explosion_vertical.java
@@ -0,0 +1,32 @@
+package uet.oop.bomberman.entities.bombEffect;
+
+import javafx.scene.image.Image;
+import uet.oop.bomberman.Map.Map;
+import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.graphics.Sprite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Explosion_vertical extends Entity {
+
+    private List<Image> explosion_vertical = new ArrayList<>();
+
+    public Explosion_vertical(double x, double y, Image img){
+        super(x,y,img);
+        explosion_vertical.add(Sprite.explosion_vertical.getFxImage());
+        explosion_vertical.add(Sprite.explosion_vertical1.getFxImage());
+        explosion_vertical.add(Sprite.explosion_vertical2.getFxImage());
+    }
+
+    public Image explosion_vertical(){
+        if(img == explosion_vertical.get(0) ) return explosion_vertical.get(1);
+        if(img == explosion_vertical.get(1) ) return explosion_vertical.get(2);
+        else return explosion_vertical.get(0);
+    }
+    @Override
+    public void update() {
+        if(Map.bomb.isEnd == true) img=explosion_vertical();
+        else img=null;
+    }
+}
