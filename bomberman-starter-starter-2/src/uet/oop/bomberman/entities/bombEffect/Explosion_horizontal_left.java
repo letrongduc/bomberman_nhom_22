12cbb21971be51bdb69735ffd73e01d438c0b666
chienltt <19020228@vnu.edu.vn>
2020-12-01 23:26:34 +0700

    Create Explosion_horizontal_left.java

---
+++ bomberman-starter-starter-2/src/uet/oop/bomberman/entities/bombEffect/Explosion_horizontal_left.java
@@ -0,0 +1,32 @@
+package uet.oop.bomberman.entities.bombEffect;
+
+import javafx.scene.image.Image;
+import uet.oop.bomberman.Map.Map;
+import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.graphics.Sprite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Explosion_horizontal_left extends Entity {
+    private List<Image> Explosion_horizontal_left = new ArrayList<>();
+
+    public Explosion_horizontal_left(double x, double y, Image img){
+        super(x,y,img);
+        Explosion_horizontal_left.add(Sprite.explosion_horizontal_right_last.getFxImage());
+        Explosion_horizontal_left.add(Sprite.explosion_horizontal_right_last1.getFxImage());
+        Explosion_horizontal_left.add(Sprite.explosion_horizontal_right_last2.getFxImage());
+    }
+
+    public Image Explosion_horizontal_left(){
+        if(img == Explosion_horizontal_left.get(0) ) return Explosion_horizontal_left.get(1);
+        if(img == Explosion_horizontal_left.get(1) ) return Explosion_horizontal_left.get(2);
+        else return Explosion_horizontal_left.get(0);
+    }
+    @Override
+    public void update() {
+        if(Map.bomb.isEnd == true) img=Explosion_horizontal_left();
+        else img=null;
+    }
+}
