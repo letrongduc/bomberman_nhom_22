511ce3fc8fe53d5e4c408367066102ada4af19d2
chienltt <19020228@vnu.edu.vn>
2020-12-01 23:26:42 +0700

    Create Explosion_vertical_down.java

---
+++ bomberman-starter-starter-2/src/uet/oop/bomberman/entities/bombEffect/Explosion_vertical_down.java
@@ -0,0 +1,30 @@
+package uet.oop.bomberman.entities.bombEffect;
+
+import javafx.scene.image.Image;
+import uet.oop.bomberman.Map.Map;
+import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.graphics.Sprite;
+
+import java.util.ArrayList;
+import java.util.List;
+public class Explosion_vertical_down extends Entity {
+    private List<Image> explosion_vertical_down = new ArrayList<>();
+
+    public Explosion_vertical_down(double x, double y, Image img){
+        super(x,y,img);
+        explosion_vertical_down.add(Sprite.explosion_vertical_down_last.getFxImage());
+        explosion_vertical_down.add(Sprite.explosion_vertical_down_last1.getFxImage());
+        explosion_vertical_down.add(Sprite.explosion_vertical_down_last2.getFxImage());
+    }
+
+    public Image explosion_vertical_down(){
+        if(img == explosion_vertical_down.get(0) ) return explosion_vertical_down.get(1);
+        if(img == explosion_vertical_down.get(1) ) return explosion_vertical_down.get(2);
+        else return explosion_vertical_down.get(0);
+    }
+    @Override
+    public void update() {
+        if(Map.bomb.isEnd == true) img=explosion_vertical_down();
+        else img=null;
+    }
+}
