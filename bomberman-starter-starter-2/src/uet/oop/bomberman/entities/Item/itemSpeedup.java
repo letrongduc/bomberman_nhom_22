fe55b31b813e3c379a7a0895c7a9f64f084b6fcd
chienltt <19020228@vnu.edu.vn>
2020-12-10 21:11:16 +0700

    Create itemSpeedup.java

---
+++ bomberman-starter-starter-2/src/uet/oop/bomberman/entities/Item/itemSpeedup.java
@@ -0,0 +1,17 @@
+package uet.oop.bomberman.entities.Item;
+
+import uet.oop.bomberman.entities.Entity;
+
+import javafx.scene.image.Image;
+
+public class itemSpeedup extends Entity {
+
+    public itemSpeedup(int x, int y, Image img)
+    {
+        super(x,y,img);
+    }
+    @Override
+    public void update() {
+
+    }
+}
